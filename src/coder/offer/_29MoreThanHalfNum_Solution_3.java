77f3c2cf73d60a1c4ed2c9ce7cbfac7e7d6fe32a
paramountNinja <paramount_zhang@163.com>
2018-01-19 17:06:21 +0800

    title 29

---
+++ src/coder/offer/_29MoreThanHalfNum_Solution_3.java
@@ -0,0 +1,55 @@
+package coder.offer;
+//@formatter:off
+ /**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *
+ * 描述：数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+ * 例如输入一个长度为9的数组{1,2,3,2,2,2,5,4,2}。
+ * 由于数字2在数组中出现了5次，超过数组长度的一半，因此输出2。如果不存在则输出0。
+ */
+ //@formatter:on
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class _29MoreThanHalfNum_Solution_3 {
+    /**
+     * 排序后，只有中间那个数可能符合条件
+     */
+    public int MoreThanHalfNum_Solution(int[] array) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            list.add(array[i]);
+        }
+        Collections.sort(list);
+        int middle = list.get(list.size() / 2);//中间那个数
+        int count = 0;
+        for (int data : list) {
+            if (data == middle)
+                count++;
+        }
+        if (count * 2 > array.length)
+            return middle;
+        else return 0;
+    }
+}
