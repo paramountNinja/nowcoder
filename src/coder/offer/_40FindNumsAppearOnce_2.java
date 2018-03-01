6c6594ca240597eab88867fac6b79bd85c10c055
paramountNinja <paramount_zhang@163.com>
2018-03-01 20:07:55 +0800

    title 40;

---
+++ src/coder/offer/_40FindNumsAppearOnce_2.java
@@ -0,0 +1,58 @@
+package coder.offer;
+//@formatter:off
+ /**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *
+ * 描述：一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ */
+ //@formatter:on
+
+ public class _40FindNumsAppearOnce_2 {
+     //时间复杂度为O(n),空间复杂度为O(1) 使用异或：相同为0，相异为1
+     public void FindNumsAppearOnce(int[] array, int num1[], int num2[]) {
+         //先异或所有的数
+         int xorResult = 0;
+         for (int i = 0; i < array.length; i++)
+             xorResult ^= array[i];
+         //找到二进制第一个1的位置，做为分组条件
+         int index = findFirst1(xorResult);
+         for (int i = 0; i < array.length; i++) {
+             if (isBit1(array[i], index))
+                 num1[0] ^= array[i];
+             else
+                 num2[0] ^= array[i];
+         }
+     }
+
+     private boolean isBit1(int target, int index) {
+         return ((target >> index) & 1) == 1;
+     }
+
+     private int findFirst1(int xorResult) {
+         int index = 0;
+         while ((xorResult & 1) == 0) {
+             xorResult >>= 1;//往右移动一位
+             index++;
+         }
+         return index;
+     }
+ }
