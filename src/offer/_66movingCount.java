3b9c397b7efa06605c482855fe66da772eb74822
paramountNinja <paramount_zhang@163.com>
2018-03-09 21:07:10 +0800

    深度遍历

---
+++ src/offer/_66movingCount.java
@@ -0,0 +1,53 @@
+package offer;
+//@formatter:off
+/**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *
+ * 描述：地上有一个m行和n列的方格。一个机器人从坐标0,0的格子开始移动，每一次只能向左，右，上，下四个方向移动一格，
+ * 但是不能进入行坐标和列坐标的数位之和大于k的格子。 例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。但是，它不能进入方格（35,38），因为3+5+3+8 = 19。请问该机器人能够达到多少个格子？
+ */
+ //@formatter:on
+public class _66movingCount {
+    public int movingCount(int threshold, int rows, int cols) {
+        int flag[][] = new int[rows][cols];//标记是否走过
+        return helper(0, 0, rows, cols, flag, threshold);//前两个代表所在点的坐标
+    }
+
+    private int helper(int i, int j, int rows, int cols, int[][] flag, int threshold) {
+        if (i < 0 || i >= rows || j < 0 || j >= cols || flag[i][j] == 1 || numSum(i) + numSum(j) > threshold) return 0;
+        flag[i][j] = 1;
+        return helper(i - 1, j, rows, cols, flag, threshold)
+                + helper(i + 1, j, rows, cols, flag, threshold)
+                + helper(i, j - 1, rows, cols, flag, threshold)
+                + helper(i, j + 1, rows, cols, flag, threshold)
+                + 1;
+    }
+
+    private int numSum(int i) {
+        int sum = 0;
+        while (i > 0) {
+            sum += i % 10;
+            i /= 10;
+        }
+        return sum;
+    }
+}
