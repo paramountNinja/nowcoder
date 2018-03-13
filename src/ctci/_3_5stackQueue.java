ea634269e8997bfa83e709e34fa364eb4fb62fbc
paramountNinja <paramount_zhang@163.com>
2018-03-13 08:33:12 +0800

    双栈实习队列

---
+++ src/ctci/_3_5stackQueue.java
@@ -0,0 +1,47 @@
+package ctci;
+//@formatter:off
+/**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *
+ * 描述：用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ */
+ //@formatter:on
+
+import java.util.Stack;
+
+public class _3_5stackQueue {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if (stack2.size() != 0) return stack2.pop();//2中有值肯定先用这里的值pop
+        while (stack1.size() != 0) {
+            int data = stack1.pop();
+            stack2.push(data);
+        }
+        return stack2.pop();
+    }
+}
