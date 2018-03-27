943d1b4548902eb5f20f31a7e533977b5b2b08f8
paramountNinja <paramount_zhang@163.com>
2018-03-27 08:29:54 +0800

    字符串翻转

---
+++ src/ctci/_1_8ReverseEqual.java
@@ -0,0 +1,53 @@
+package ctci;
+//@formatter:off
+import java.util.Arrays; /**
+ *                             _ooOoo_
+ *                            o8888888o
+ *                            88" . "88
+ *                            (| -_- |)
+ *                            O\  =  /O
+ *                         ____/`---'\____
+ *                       .'  \\|     |//  `.
+ *                      /  \\|||  :  |||//  \
+ *                     /  _||||| -:- |||||-  \
+ *                     |   | \\\  -  /// |   |
+ *                     | \_|  ''\---/''  |   |
+ *                     \  .-\__  `-`  ___/-. /
+ *                   ___`. .'  /--.--\  `. . __
+ *                ."" '<  `.___\_<|>_/___.'  >'"".
+ *               | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+ *               \  \ `-.   \_ __\ /__ _/   .-` /  /
+ *          ======`-.____`-.___\_____/___.-`____.-'======
+ *                             `=---='
+ *          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+ *                     佛祖保佑        永无BUG
+ *
+ * 描述：假定我们都知道非常高效的算法来检查一个单词是否为其他字符串的子串。请将这个算法编写成一个函数，给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成，要求只能调用一次检查子串的函数。
+ * 给定两个字符串s1,s2,请返回bool值代表s2是否由s1旋转而成。字符串中字符为英文字母和空格，区分大小写，字符串长度小于等于1000。
+ */
+ //@formatter:on
+public class _1_8ReverseEqual {
+    public boolean checkReverseEqual(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        char[] chars1 = s1.toCharArray();
+        char[] chars2 = s2.toCharArray();
+        Arrays.sort(chars1);
+        Arrays.sort(chars2);
+        for (int i = 0; i < chars1.length; i++) {
+            if (chars1[i] != chars2[i])
+                return false;
+        }
+        return true;
+
+    }
+
+    //法二：将s1变成s1s1，若s2是翻转子串一定被包含
+    public boolean checkReverseEqual_2(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        String str = s1 + s1;
+        if (str.contains(s2))
+            return true;
+        else return false;
+
+    }
+}
